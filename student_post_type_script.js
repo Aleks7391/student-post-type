802f9574eb266e0daa2597faa39714c8f9cadbbd
Aleks7391 <aleks.ganev7@gmail.com>
2021-05-27 17:42:23 +0300

    Added ajax for the checkboxes in the student admin page functionality

---
+++ student_post_type_script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function () {
+    jQuery('.ag_status_meta_admin').each(function () {
+        jQuery(this).val(this.checked);
+
+        let post_id_text = jQuery(this).parent().parent().attr('id');
+
+        let post_id = post_id_text.replace(/^\D+/g, '');
+        jQuery(this).change(function () {
+            jQuery.ajax({
+                type: "POST",
+                url: myAjax.ajaxurl,
+                data: { action: 'ag_update_active_status', post_id: post_id },
+            });
+        })
+    })
+})
